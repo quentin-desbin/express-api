cd6af4df7a2f564c5392e53aa3ea832b7838ff59
Quentin DESBIN <quentin.desbin@ynov.com>
2025-04-22 10:40:16 +0200

    :sparkles: add openapi validator

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ import express from 'express'
 import usersRouter from './routers/users.router.js'
 import errorMiddleware from './middlewares/error.middleware.js'
 import demoMiddleware from './middlewares/demo.middleware.js'
+import OpenApiValidator from 'express-openapi-validator'
 
 const app = express()
 app.use(express.json())
 
 // Use a global customized standard middleware
 app.use(demoMiddleware)
+
+app.use(
+    OpenApiValidator.middleware({
+        apiSpec: './openapi-main.yaml',
+        validateRequests: true,
+        validateResponses: false
+    })
+)
 
 app.use('/users', usersRouter)
 
